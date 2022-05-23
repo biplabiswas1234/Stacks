c88d7b7b59fcbcb7aedc87ecff88f9119b6c6fac
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-23 20:56:47 +0530

    [Biplab] Add: Creating a stack

---
+++ StackData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
